0755fc1bc2ddd516883d20516efaac42aac87ddb
Kyle Kleven <kylekleven8@gmail.com>
2021-04-06 00:31:45 -0500

    Added GET by post id, comment route, and improved site styling.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+//POST create post
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -15,6 +16,14 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+//GET post by id
+router.get('/:id', (req, res) => {
+  res.render('post', {
+      loggedIn: req.session.logged_in
+  });
+});
+
+//DELETE post
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
@@ -32,6 +41,40 @@ router.delete('/:id', withAuth, async (req, res) => {
     res.status(200).json(PostData);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//PUT update post
+router.put('/:id', async (req, res) => {
+  try {
+      const postData = await Post.update({
+          title: req.body.title,
+          description: req.body.description
+      }, {
+          where: {
+              id: req.body.post_id,
+          }
+      });
+      if (!postData) {
+          res.status(404).json({ message: "No posts found with that ID!" });
+      }
+      res.status(200).json(postData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
+//POST create a comment
+router.post("/comment", async (req, res) => {
+  try {
+      const newComment = await Comment.create({
+          description: req.body.description,
+          post_id: req.body.post_id,
+          user_id: req.session.user_id
+      });
+      res.status(200).json(newComment);
+  } catch (err) {
+      res.status(400).json(err);
   }
 });
 
